edeecae3f61d9da3c6e5ba2f79f23edc137461ff
Oleg Shchegolev <shegolev@beta.agency>
2022-10-25 09:42:20 +0300

    chore: [nx migration] update-jest-config-to-use-util

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/apps/rickandmorty',
     '<rootDir>/libs/ngx-ssr/cache',
     '<rootDir>/libs/rickandmorty/api',
